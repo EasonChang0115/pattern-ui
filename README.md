0efd3156e3e205097ad3dd0e08d7361ea169fbfa
EasonChang <s8710606@gmail.com>
2021-10-19 13:32:56 +0800

    style: 樣式導入

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# pattern-develop-ui
+# Pattern GO UI 介面
 
-Pattern GO 內部 UI 介面
 
-開發時，需注意要打包 dist 才能上到 gitlab
-
-v0.0.34
-設計系統的 input number 加上 focus 樣式
-設計系統的 input number 加上寬度設定
-selector 組件修正 options 預先設定 v-if 改成 v-show
-新增設計系統的 selector 組件
-設計系統的 selector 修改 box-shahow
-
-v0.0.35
-
-新增設計系統 switcher 組件
-調整 designInputNumber onChange 觸發時機
-
-v0.0.45
-
-新增設計系統 slider 組件
-新增 message 組件
-
-v0.0.67
-
-fix: 新增判斷條件 避免初始化時 最大最小值與 currentValue 相同 卻觸發 change
-feature: 新增 switch 組件樣式
--- vue.config.js
+++ vue.config.js
@@ -3,17 +3,17 @@ module.exports = {
     loaderOptions: {
       scss: {
         prependData: `
-          @import "~pattern-scss/variables.scss";
-          @import "~pattern-scss/mixins/text-truncate.scss";
-          @import "~pattern-scss/mixins/flex-center.scss";
-          @import "~pattern-scss/mixins/clearfix.scss";
-          @import "~pattern-scss/mixins/breakpoints.scss";
-          @import "~pattern-scss/mixins/triangle.scss";
-        `
-      }
-    }
+          @import "@/scss/_variables.scss";
+          @import "@/scss/mixins/_text-truncate.scss";
+          @import "@/scss/mixins/_flex-center.scss";
+          @import "@/scss/mixins/_clearfix.scss";
+          @import "@/scss/mixins/_breakpoints.scss";
+          @import "@/scss/mixins/_triangle.scss";
+        `,
+      },
+    },
   },
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     // 壓縮圖片
     config.module
       .rule('images')
@@ -22,18 +22,18 @@ module.exports = {
       .options({
         mozjpeg: {
           progressive: true,
-          quality: 65
+          quality: 65,
         },
         optipng: {
-          enabled: false
+          enabled: false,
         },
         pngquant: {
           quality: [0.1, 0.5],
-          speed: 4
+          speed: 4,
         },
         gifsicle: {
-          interlaced: false
-        }
+          interlaced: false,
+        },
         // webp: { quality: 75 }
       });
   },
@@ -44,7 +44,7 @@ module.exports = {
       maxAssetSize: 30000000,
       assetFilter(assetFilename) {
         return assetFilename.endsWith('.js');
-      }
-    }
-  }
+      },
+    },
+  },
 };
